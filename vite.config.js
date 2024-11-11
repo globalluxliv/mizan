ad9eaf1744f0ab66fc85bf5935e74de1a0e993a1
freddycmh <50223798+freddycmh@users.noreply.github.com>
2024-11-11 16:16:33 -0500

    chnage vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/mizan-humanity/', // Replace 'mizan-humanity' with your GitHub repository name
+  base: '/mizan/', 
   plugins: [react()],
 })
